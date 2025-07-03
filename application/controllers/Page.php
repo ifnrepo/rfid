9168c9dd7e36132c38231e3553868ad2c8471a36
agun0042 <agun0042@student.monash.edu>
2025-07-03 10:08:41 +0700

    bentar lagi kelar

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -1,52 +1,76 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Page extends CI_Controller {
+class Page extends CI_Controller
+{
 
-    public function __construct()
-    {
-        parent::__construct();
-        // $this->load->library('form_validation');
-        // $this->load->library('session');
-        // $this->load->model('userappsmodel');
-        $this->load->model('page_model', 'pagemodel');
-        // $this->load->model('helper_model', 'helpermodel');
-        // if(get_cookie('bantuMasukMomois')=='YES'){
-        //     $this->_loginwithcookie(get_cookie('usernameMasukMomois'),get_cookie('passwordMasukMomois'));
-        // }
-    }
-	public function fn(){
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->library('form_validation');
+		// $this->load->library('session');
+		// $this->load->model('userappsmodel');
+		$this->load->model('page_model', 'pagemodel');
+		// $this->load->model('helper_model', 'helpermodel');
+		// if(get_cookie('bantuMasukMomois')=='YES'){
+		//     $this->_loginwithcookie(get_cookie('usernameMasukMomois'),get_cookie('passwordMasukMomois'));
+		// }
+	}
+	public function fn()
+	{
 		$footer['halaman'] = 'fn';
-		$this->session->set_userdata('sesifn',time());
+		$this->session->set_userdata('sesifn', time());
 		$this->load->view('layouts/header');
 		$this->load->view('infin.php');
-		$this->load->view('layouts/footer',$footer);
+		$this->load->view('layouts/footer', $footer);
 	}
-	public function gf(){
+	public function gf()
+	{
 		$footer['halaman'] = 'gf';
-		$this->session->set_userdata('sesigf',time());
+		$this->session->set_userdata('sesigf', time());
 		$this->load->view('layouts/header');
 		$this->load->view('ingf.php');
-		$this->load->view('layouts/footer',$footer);
+		$this->load->view('layouts/footer', $footer);
 	}
-	public function box(){
+	public function box()
+	{
 		$footer['halaman'] = 'bx';
+		$data['pl_list'] = $this->pagemodel->getplno();
+		$plno = $this->input->post('selectedPlNo', true);
+		if ($plno === null && !empty($data['pl_list'])) {
+			$plno = $data['pl_list'][0]['plno'];
+		}
+		$data['plno'] = $plno;           // so view can use $plno
+		$data['selectedPlNo'] = $plno;
+
+		$showDone = $this->input->post('show_done', true) === '1';
+
+		if ($showDone) {
+			$data['orders'] = $this->pagemodel->getOrderByPlNoDone($plno);
+		} else {
+			$data['orders'] = $this->pagemodel->getOrderByPlNo($plno);
+		}
+
+		// **Pass it to the view**
+		$data['show_done'] = $showDone;
+
 		$this->load->view('layouts/header');
-		$this->load->view('outbox.php');
-		$this->load->view('layouts/footer',$footer);
+		$this->load->view('outbox.php', $data);
+		$this->load->view('layouts/footer', $footer);
 	}
-	public function validateHandheld(){
+	public function validateHandheld()
+	{
 		$data = $_POST['data'];
-		$x=0;
-		foreach($data as $dat=>$value){
+		$x = 0;
+		foreach ($data as $dat => $value) {
 			$hasil = $this->pagemodel->cekdatafn($value['value']);
-			if(count($hasil)> 0 ){
+			if (count($hasil) > 0) {
 				$data[$x]['value'] = $hasil['isi'];
 				$data[$x]['status'] = $hasil['status'];
 				$data[$x]['done'] = $hasil['done'];
 
-			}else{
-				$data[$x]['value'] = $value['value'].' - (NOT FOUND)';
+			} else {
+				$data[$x]['value'] = $value['value'] . ' - (NOT FOUND)';
 				$data[$x]['status'] = 'NG';
 				$data[$x]['done'] = 'NOT FOUND';
 			}
@@ -54,22 +78,54 @@ class Page extends CI_Controller {
 		}
 		echo json_encode($data);
 	}
-	public function validateingudang(){
+	public function validateingudang()
+	{
 		$data = $_POST['data'];
-		$x=0;
-		foreach($data as $dat=>$value){
+		$x = 0;
+		foreach ($data as $dat => $value) {
 			$hasil = $this->pagemodel->cekDataIn($value['value']);
-			if(count($hasil) > 0 ){
+			if (count($hasil) > 0) {
 				$data[$x]['value'] = $hasil['isi'];
 				$data[$x]['status'] = $hasil['status'];
 				$data[$x]['done'] = $hasil['done'];
-			}else{
-				$data[$x]['value'] = $value['value'].' - (NOT FOUND)';
+			} else {
+				$data[$x]['value'] = $value['value'] . ' - (NOT FOUND)';
 				$data[$x]['status'] = 'NG';
 				$data[$x]['done'] = 'TIDAK DI GUDANG';
 			}
 			$x++;
 		}
 		echo json_encode($data);
+	}
+
+	public function validateContainer()
+	{
+		$plno = $this->input->post('selectedPlNo', true);
+		$scans = $this->input->post('data', true);
+		$out = [];
+
+		foreach ($scans as $scan => $s) {
+			$epc = $s['value'];
+
+			$res = $this->pagemodel->checkRFIDForBale($epc, $plno);
+
+			if (is_array($res) && isset($res['isi'], $res['status'])) {
+				$out[$scan] = [
+					'key' => $scan + 1,
+					'value' => $res['isi'],
+					'status' => $res['status'],
+					'done' => '',            // you can choose to include a done-message
+				];
+			} else {
+				// otherwise assume $res is a string message and show NG
+				$out[$scan] = [
+					'key' => $scan + 1,
+					'value' => $epc,
+					'status' => 'NG',
+					'done' => is_string($res) ? $res : 'NG',
+				];
+			}
+		}
+		echo json_encode(array_values($out));
 	}
 }
--- application/views/outbox.php
+++ application/views/outbox.php
@@ -1,3 +1,9 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<?php
+// make sure $show_done always exists
+$show_done = isset($show_done) && $show_done;
+?>
+
 <section class="resume-section p-3" style="align-items : normal !important" id="about">
     <div class="resume-section-content">
         <div class="row">
@@ -8,43 +14,126 @@
                 </h5>
             </div>
             <div class="col-sm-6 float-right pt-1" style="text-align: right;">
-                <select class="form-control form-select btn-sm flat font-kecil" id="alat" name="alat" style="width: 60% !important; float: right; height: 31px; margin-right: 5px;">
+                <select class="form-control form-select btn-sm flat font-kecil" id="alat" name="alat"
+                    style="width: 60% !important; float: right; height: 31px; margin-right: 5px;">
                     <option value="fixedreader">Fixed Reader</option>
                     <option value="handheld">Handheld</option>
                 </select>
             </div>
         </div>
-        <div class="col-sm-6 float-right pt-1" style="text-align: left;">
-            <h5>Pilih PO</h5>
-            <select class="form-control form-select btn-sm flat font-kecil" id="alat" name="alat" style="width: 60% !important; float: left; height: 31px; margin-right: 5px;">
+        <form action="<?= site_url('page/box') ?>" method="post" class="row align-items-center mb-3">
 
-                <!-- Tp ini harus nge itterate database yang ada -->
-                <option value="fixedreader">PO1</option>
-                <option value="handheld">PO2</option>
-            </select>
-        </div>
+            <div class="row align-items-center mb-3">
+                <div class="col-sm-6 float-right pt-1" style="text-align: left;">
+                    <h5>Pilih PlNO</h5>
+                    <select class="form-control form-select btn-sm flat font-kecil" id="plSelect" name="selectedPlNo"
+                        style="width: 60% !important; float: left; height: 31px; margin-right: 5px;"
+                        onchange="this.form.submit()">
+
+                        <option value="">-- Pilih PL No --</option>
+                        <?php foreach ($pl_list as $row):
+                            $sel = ($row['plno'] === $selectedPlNo); ?>
+                            <option value="<?= html_escape($row['plno']) ?>" <?= $sel ? 'selected' : '' ?>>
+                                <?= html_escape($row['plno']) ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="col-md-6" id="inputanrf">
+                    <label for="inputrfid" class="col-sm-2 col-form-label pt-1 font-bold"
+                        style="height: 26px;">RFID</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control input-sm flat p-0" id="inputrfid">
+                    </div>
+                </div>
+            </div>
+        </form>
 
 
-        <!-- add space  -->    
+
+        <!-- add space  -->
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <div class="py-3"><!-- empty, adds vertical gap --></div>
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-lg-6 col-md-8">
+            <div class="col-lg-6 col-md-8 d-flex flex-column" style="height: 500px;">
+                <div class="d-flex justify-content-end mb-2">
+                    <form method="post" action="<?= site_url('page/box') ?>" style="display:inline;">
+                        <!-- preserve the selected PL No -->
+                        <input type="hidden" name="selectedPlNo" value="<?= html_escape($plno) ?>">
+                        <?php if ($show_done): ?>
+                            <!-- If we’re showing done, switch back to pending -->
+                            <input type="hidden" name="show_done" value="0">
+                            <button type="submit" class="btn btn-sm btn-primary">
+                                Daftar order belum selesai
+                            </button>
+                        <?php else: ?>
+                            <!-- Otherwise show the done list -->
+                            <input type="hidden" name="show_done" value="1">
+                            <button type="submit" class="btn btn-sm btn-primary">
+                                Daftar order selesai
+                            </button>
+                        <?php endif; ?>
+                    </form>
+                </div>
                 <div class="d-flex align-items-center mb-1">
                     <div class="flex-grow-1 font-bold">INPUT LIST</div>
-                    <div class="text-end font-bold" style="width:100px;">STATUS</div>
+                    <div class="text-end font-bold" id="posex" style="width:100px;">STATUS</div>
                 </div>
-                <hr class="my-1"/>
-                <div id="tampungan">
-                <!-- content si isi nya ada disini -->
+                <hr class="my-1" />
+                <div id="tampungan" class="flex-grow-1 overflow-auto border rounded p-2">
+                    <?php if (empty($orders)): ?>
+                        <div class="text-center text-muted">Tidak ada data untuk PL: <?= html_escape($plno) ?></div>
+                    <?php else: ?>
+                        <?php foreach ($orders as $o): ?>
+
+                            <div class="row align-items-center mb-1">
+                                <div class="col-7 text-start">
+                                    <?= $o['id'] ?>. <?= html_escape($o['po']) ?>
+                                    / <?= html_escape($o['item']) ?>
+                                    <?php if ($o['dis']): ?> dis <?= $o['dis'] ?><?php endif; ?>
+                                    Bale <?= $o['nobale'] ?>
+                                </div>
+                                <div class="col-2 text-center">
+                                    <?= $o['masuk'] ?>
+                                </div>
+                                <div class="col-3 d-flex justify-content-end">
+                                    <!-- if you have a status field in $o (e.g. OK/NG) -->
+                                    <?php
+                                    if (empty($o['masuk'])) {
+                                        $st = 'NG';
+                                    } else {
+                                        $st = $o['status'] ?? 'OK';
+                                    }
+
+                                    // then map it to a Bootstrap badge color
+                                    $cls = $st === 'OK'
+                                        ? 'success'
+                                        : ($st === 'SA'
+                                            ? 'warning'
+                                            : 'danger');
+                                    ?>
+                                    <span class="badge bg-<?= $cls ?>"><?= $st ?></span>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="py-3"><!-- empty, adds vertical gap --></div>
+                </div>
+            </div>
+            <div class="progress w-50 mt-2">
+                <div id="scanProgress" class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0"
+                    aria-valuemin="0" aria-valuemax="100">
+                    10%
                 </div>
             </div>
         </div>
-
-        
-
     </div>
 </section>
